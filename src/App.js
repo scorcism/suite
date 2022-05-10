123c742181110472211952d82491f4197ac613d7
scorcism <abhishekpathak1720@gmail.com>
2022-05-10 11:54:26 +0530

    Initially done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [userName, setUserName] = useState("")
+  const [userPassword,setUserPassword] = useState(0)
+
+  const submit = () =>{
+    let name = userName
+    let pwd = userPassword
+    let h2 = document.getElementById("h2")
+    if(name == "abhishek" & pwd == 1234){
+      h2.innerHTML = `Password Matched Pal <span> Passwrod: ${pwd}</span>`
+      setUserName("")
+      setUserPassword("")
+    }else{
+      h2.innerHTML = `Password Does not matched Pal <span>check console</span>`
+      console.log("userName: abhishek \nuserPassword: 1234")
+      setUserName("")
+      setUserPassword("")
+    }
+  }
+
+
   return (
     <div className="App">
+      <h2 id='h2'>For Burp Suite Testing</h2>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Enter your name: <input id="name" onChange={(e) =>setUserName(e.target.value)} value = {userName} type="text" /> <br/><br/>
+        Enter your password: <input placeholder='number only' onChange={(e) => setUserPassword(e.target.value)} value = {userPassword} id="password" min="1" max="10000" type="number" />
+       <small>Name: {userName} <br/> Password: {userPassword} </small>
+        <input onClick={submit} type="submit" value="submit"/>
       </header>
     </div>
   );
